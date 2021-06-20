bcb20f1d784eb4803a69230ce4719c9bd4afb7cd
siddhartth1987 <siddhartth.1987@gmail.com>
2021-06-20 10:48:46 +0530

    Added header and minor styling to Profile Page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './home/Home';
 import Profile from './profile/Profile';
+import Details from './details/Details';
+import Checkout from './checkout/Checkout';
 
 // Controller class for routing to various pages.
 class Controller extends Component{
@@ -20,6 +22,12 @@ class Controller extends Component{
 
                     {/* Profile Page */}
                     <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
+
+                    {/* Details Page */}
+                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+
+                    {/* Checkout Page */}
+                    <Route path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
 
                 </div>
             </Router>
